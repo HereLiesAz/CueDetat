027d8c5bdcf6ab096f9c866c78b92b2edd19c425
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-03-28 18:54:51 -0500

    Update CvReducer.kt

--- app/src/main/java/com/hereliesaz/cuedetat/domain/reducers/CvReducer.kt
+++ app/src/main/java/com/hereliesaz/cuedetat/domain/reducers/CvReducer.kt
@@ -81,7 +81,7 @@ internal fun reduceCvAction(state: CueDetatState, action: MainScreenEvent): CueD
                     }
                 } else {
                     nextState = nextState.copy(
-                        arConfidenceHistory = history,
+                        arConfidenceHistory = history.toList(),
                         arLowConfidenceFrameCount = 0
                     )
                 }
